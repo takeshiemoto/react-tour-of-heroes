63b747ab00387a51b7d684549d97b2e75549dc66
Takeshi Emoto <takeshi.emoto@jointcrew.co.jp>
2020-12-25 14:22:10 +0900

    dev

--- apps/tour-of-heroes/pages/index.tsx
+++ apps/tour-of-heroes/pages/index.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
 
 import { APP_NAME } from '../environments';
+import useSWR from 'swr';
+import { Hero } from '../types';
 
 export function Index() {
-  return <h1>{APP_NAME}</h1>;
+  const fetcher = (url) => fetch(url).then((res) => res.json());
+
+  const { data: heroes, error } = useSWR<Hero[]>(`/api/v1/heroes`, fetcher);
+
+  return (
+    <>
+      <h1>{APP_NAME}</h1>
+      <h2>Dashboard</h2>
+      <p>Client Side Rendering</p>
+      {error && <div>Fetch data error</div>}
+      {heroes && (
+        <ul>
+          {heroes.map((h) => (
+            <li key={h.id}>
+              ID: {h.id} Name: {h.name}
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
 }
 
 export default Index;
