cd20f821d865fbd42c5e9f2158b7f19524867a98
Takeshi Emoto <takeshi.emoto@jointcrew.co.jp>
2020-12-28 14:04:07 +0900

    dev

--- apps/tour-of-heroes-admin/next.config.js
+++ apps/tour-of-heroes-admin/next.config.js
@@ -2,4 +2,8 @@ const withNx = require('@nrwl/next/plugins/with-nx');
 
 module.exports = withNx({
   target: 'serverless',
+  env: {
+    API_URL: process.env.API_URL,
+    DEV_API_URL: process.env.DEV_API_URL,
+  },
 });
